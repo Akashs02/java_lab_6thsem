1d842248f85e9526b3ebe22034b25486ff6ae59f
A K A S H <76429075+Akashs02@users.noreply.github.com>
2022-04-26 08:52:36 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package BookDatabase;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BookList bl = new BookList();
+		bl.getBooks("a1");
+		bl.getBookswithPrice(10);
+		bl.getBooksWithPublisher("p1");
+		bl.getBookWithTitle("t1");
+		bl.SortPrice();
+		bl.updateBook("t1");
+		bl.display();
+	}
+
+}
